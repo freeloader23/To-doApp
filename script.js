5fca3eed628266e951092b01c713901ab5949150
Nischal <sharmanischal823@gmail.com>
2025-04-06 10:36:20 +0530

    Script commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNote() {
+    const input = document.getElementById("noteInput");
+    const text = input.value.trim();
+    if (text) {
+      const div = document.createElement("div");
+      div.className = "note";
+      div.textContent = text;
+      document.getElementById("notes").appendChild(div);
+      input.value = "";
+    }
+  }
+  
