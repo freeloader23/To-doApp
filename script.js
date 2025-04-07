8198ce888aae7987f1ccf0dfb5c2775b2cab9b2b
Nischal <sharmanischal823@gmail.com>
2025-04-07 10:54:39 +0530

    Edit Task added

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function addNote() {
-    const input = document.getElementById("noteInput");
-    const text = input.value.trim();
-    if (text) {
-      const div = document.createElement("div");
-      div.className = "note";
-      div.textContent = text;
-      document.getElementById("notes").appendChild(div);
-      input.value = "";
-    }
+  const input = document.getElementById("noteInput");
+  const text = input.value.trim();
+  if(text) {
+    const div = document.createElement("div");
+    div.className = "note";
+    div.textContent = text;
+    document.getElementById("notes").appendChild(div);
+    input.value = "";
   }
-  
+}
